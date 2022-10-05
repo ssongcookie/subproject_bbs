e3f8661511fdc453e2346a40fa1db835dbce562a
song <sgh83037@naver.com>
2022-10-05 13:23:29 +0900

    게시글 상세페이지 구현  - 상세페이지 가져오기

--- BBS/src/main/java/bbs/BbsDAO.java
+++ BBS/src/main/java/bbs/BbsDAO.java
@@ -125,4 +125,28 @@ public class BbsDAO {
 		return false;
 		
 	}
+	
+	//게시글 상세페이지
+	public Bbs getBbs(int bbsID) {
+		String SQL = "SELECT * FROM BBS WHERE bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(2));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				return bbs;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;	//글이 존재하지 않는 경우 null 반환
+	}
 }
